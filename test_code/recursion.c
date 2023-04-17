ef8e59b302ab9197500252fff0538aca3d71ea7c
TomSavas <tomas.savickas@protonmail.com>
2023-04-18 00:08:57 +0300

    Fix recursion test

--- test_code/recursion.c
+++ test_code/recursion.c
@@ -3,24 +3,24 @@
 
 int fib(int n, bool deep) {
     static bool deep_once_hit = false;
-    if (deep && !deep_once_hit && n == 0) {
-        // Just something to put a bp on with a deep stack
-        printf("Deep!\n");
-        deep_once_hit = true;
-        return 0;
-    }
-
     if (n < 2) 
         return n;
 
     int a = fib(n - 1, deep);
     int b = fib(n - 2, deep);
 
+    if (deep && !deep_once_hit && n == 2) {
+        // Just something to put a bp on with a deep stack
+        printf("Deep!\n");
+        deep_once_hit = true;
+        return 0;
+    }
+
     return a + b;
 }
 
 int main(int argc, char** argv) {
-    int n = 4;
+    int n = 16;
     for (int i = 0; i <= n; ++i) {
         int f = fib(i, i == n);
         printf("%dth of Fib = %d\n", i, f);
